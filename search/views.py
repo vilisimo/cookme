bb15d618bb848e94d8e8e83e23723ec6ab833ddf
vilisimo <vilisimo@gmail.com>
2016-10-21 18:03:09 +0100

    Now accesses query string. Next step: filtering.

--- search/views.py
+++ search/views.py
@@ -7,5 +7,9 @@ from django.shortcuts import render
 
 def search_results(request):
     # request.GET.get['q'] to get query string
-    content = {}
+    ingredients = request.GET.get('q').split()
+    ingredients = [ingredient.strip() for ingredient in ingredients]
+    content = {
+        'ingredients': ingredients
+    }
     return render(request, 'search/search_results.html', content)
