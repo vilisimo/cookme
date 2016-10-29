d615e88ecdeda651edb13092fc7f07e5d5770b38
vilisimo <vilisimo@gmail.com>
2016-10-29 01:30:31 +0100

    Restructured helper code. Fridge search makes more sense in... Fridge app...

--- search/helpers.py
+++ search/helpers.py
@@ -4,9 +4,6 @@ Helper functions to support functionality related to searching the recipes.
 
 from string import capwords
 
-from recipes.models import Recipe
-from ingredients.models import Ingredient
-
 
 def encode(query):
     """
@@ -66,33 +63,3 @@ def get_name_set(decoded_query):
 
     names = set([capwords(name) for name in decoded_query.split(',')])
     return names
-
-
-def get_fridge_recipes(ingredients):
-    """
-    Returns a list of recipes that contain ingredients from a list and
-    nothing more (but they do not have to have every ingredient).
-
-    Logic: first of all, get all ingredients that are not found in a set.
-    This is a list of ingredients that we do not want recipes to contain.
-    Once we have this list, we can select only those recipes that do not
-    contain any of these ingredients, leaving us with a query set of recipes
-    that actually contain only those ingredients that we are interested in.
-
-    :param ingredients: a set of CAPITALIZED ingredients of which at least one
-                        should be in a recipe.
-    :return: QuerySet of recipe objects that have at least one ingredient.
-    """
-
-    # Ensures set contents are capitalized, as DB expects capitalized names.
-    # Should never need to capitalize ingredients, but just in case it happens.
-    ingredients = [ingredient if ingredient.istitle() else
-                   capwords(ingredient) for ingredient in ingredients]
-
-    # Get all the ingredients that do not match our set.
-    non_matching = Ingredient.objects.exclude(name__in=ingredients)
-
-    # Now get all recipes that do not have these ingredients.
-    recipes = Recipe.objects.exclude(ingredients__in=non_matching)
-
-    return recipes
--- search/tests/test_helpers.py
+++ search/tests/test_helpers.py
@@ -3,11 +3,10 @@ Tests to ensure that helper functions are fully operational.
 """
 
 from django.test import TestCase
-from django.contrib.auth.models import User
 
-from search.helpers import encode, decode, get_name_set, get_fridge_recipes
-from ingredients.models import Unit, Ingredient
-from recipes.models import Recipe, RecipeIngredient as RecIng
+
+from search.helpers import encode, decode, get_name_set
+
 
 
 class EncodingTests(TestCase):
@@ -128,83 +127,4 @@ class GetNameSetExtractionTests(TestCase):
         expected = {'Multiple Words', 'Ingredient', 'Ingredient 2'}
 
         self.assertEqual(names, expected)
-
-
-class TestGetRecipes(TestCase):
-    """
-    Test cases to ensure that matching recipes against a given ingredient set
-    returns an expected result.
-    """
-
-    def setUp(self):
-        user = User.objects.create(username='get_fridge_recipes')
-
-        # Ingredients
-        i1 = Ingredient.objects.create(name='Meat', type='Meat')
-        i2 = Ingredient.objects.create(name='Lemon', type='Fruit')
-        i3 = Ingredient.objects.create(name='Apple', type='Fruit')
-        i4 = Ingredient.objects.create(name='White Bread', type='Bread')
-        # Units
-        u = Unit.objects.create(name='kilogram', abbrev='kg')
-
-        # Recipes
-        self.r = Recipe.objects.create(author=user, title='MeatRec')
-        RecIng.objects.create(recipe=self.r, ingredient=i1, unit=u, quantity=1)
-
-        self.r2 = Recipe.objects.create(author=user, title='MeatLemonAppleRec')
-        RecIng.objects.create(recipe=self.r2, ingredient=i1, unit=u, quantity=1)
-        RecIng.objects.create(recipe=self.r2, ingredient=i2, unit=u, quantity=1)
-        RecIng.objects.create(recipe=self.r2, ingredient=i3, unit=u, quantity=1)
-
-        self.r3 = Recipe.objects.create(author=user, title="AllIngredientsRec")
-        RecIng.objects.create(recipe=self.r3, ingredient=i1, unit=u, quantity=1)
-        RecIng.objects.create(recipe=self.r3, ingredient=i2, unit=u, quantity=1)
-        RecIng.objects.create(recipe=self.r3, ingredient=i3, unit=u, quantity=1)
-        RecIng.objects.create(recipe=self.r3, ingredient=i4, unit=u, quantity=1)
-
-        self.r4 = Recipe.objects.create(author=user, title="LemonRec")
-        RecIng.objects.create(recipe=self.r4, ingredient=i2, unit=u, quantity=1)
-
-    def test_get_recipes_one_ingredient(self):
-        """
-        Ensures that when only one ingredient is given, only a recipe
-        with that one ingredient is returned.
-        """
-
-        ingredients = {'meat'}
-        recipes = get_fridge_recipes(ingredients)
-
-        self.assertIn(self.r, recipes)
-
-    def test_get_recipes_two_ingredients(self):
-        """
-        Ensures that when we there are two ingredients in a set, only those
-        recipes that consist of one ingredient or both of them are returned.
-        """
-
-        ingredients = {'meat', 'lemon'}
-        recipes = get_fridge_recipes(ingredients)
-        expected = [self.r, self.r4]
-
-        self.assertEquals(expected, list(recipes))
-
-    def test_get_recipes_all_ingredients(self):
-        """
-        Ensures that all recipes are chosen when ingredient list includes all
-        possible ingredients.
-        """
-
-        ingredients = {'meat', 'lemon', 'apple', 'white bread'}
-        recipes = get_fridge_recipes(ingredients)
-        expected = Recipe.objects.all()
-
-        self.assertEquals(list(expected), list(recipes))
-
-    def test_non_existent_ingredient(self):
-        """ Ensure that non-existent ingredients do not return anything. """
-
-        ingredients = {'fairy dust'}
-        recipes = get_fridge_recipes(ingredients)
-
-        self.assertFalse(recipes)
 
