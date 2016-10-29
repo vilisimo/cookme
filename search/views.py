9b4657cc6023593091a14ce61d6018b2fa1c3828
vilisimo <vilisimo@gmail.com>
2016-10-29 01:23:55 +0100

    Cleaner way to get matching recipes. Needs testing, though.

--- search/views.py
+++ search/views.py
@@ -3,6 +3,7 @@ Logic related to searching fridge & global recipes lives.
 """
 
 from django.shortcuts import render
+from django.db.models import Q
 
 from recipes.models import Recipe
 from .helpers import decode, get_name_set
@@ -10,7 +11,9 @@ from .helpers import decode, get_name_set
 
 def search_results(request):
     """
-    Shows the results of a search from a main page.
+    Shows a list of recipes that have the ingredients entered.
+
+    Note: ingredients entered are a subset of a recipe, not a superset.
 
     :param request: default request object.
     :return: default render object.
@@ -22,11 +25,13 @@ def search_results(request):
         ingredients = decode(ingredients)
         ingredients = get_name_set(ingredients)
 
-        recipes = Recipe.objects.all()
-        for recipe in recipes:
-            recipe_ings = set([ing.name for ing in recipe.ingredients.all()])
-            if ingredients.issubset(recipe_ings):
-                matched.append(recipe)
+        # # In case testing reveals something fishy:
+        # recipes = Recipe.objects.all()
+        # for recipe in recipes:
+        #     recipe_ings = set([ing.name for ing in recipe.ingredients.all()])
+        #     if ingredients.issubset(recipe_ings):
+        #         matched.append(recipe)
+        matched = Recipe.objects.exclude(~Q(ingredients__name__in=ingredients))
 
     content = {
         'ingredients': ingredients,
