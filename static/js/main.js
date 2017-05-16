d013796316a4d978e42b25bb485be9172aae6531
vilisimo <vilisimo@gmail.com>
2017-05-16 20:52:25 +0300

    Add check to execute js only in home page, change font

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,13 @@
-function focus_searchbar() {
-    const search_bar = document.getElementById('id_q');
-    if (search_bar !== null) {
-        search_bar.focus();
+function get_body() {
+    return document.getElementsByTagName('body')[0];
+}
+
+function focusSearch() {
+    const body = get_body();
+    if (body.className === 'home') {
+        const search_bar = document.getElementById('id_q');
+        if (search_bar !== null) {
+            search_bar.focus();
+        }
     }
 }
