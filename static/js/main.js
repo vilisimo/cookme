2c82bba77783e462165cf2e57cbbb69dcc7fcd8f
vilisimo <vilisimo@gmail.com>
2017-06-26 20:30:39 +0300

    Refactor event application

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,31 @@
-function Element(id, charsLeftContainer) {
+"use strict";
+
+function InputArea(id, charsLeftContainer, addEventFunction) {
     this.elementId = id;
     this.charsContainer = charsLeftContainer;
+    this.applyEvent = addEventFunction;
 }
 
 $(function() {
-    const title = new Element('id_title', 'title_chars_left');
-    const description = new Element('id_description', 'description_chars_left');
-    const steps = new Element('id_steps', 'steps_chars_left');
-
-    addCharsLeftEventTo(title);
-    addCharsLeftEventTo(description);
-    addCharsLeftEventTo(steps);
+    const elements = createElements();
+    addEventsTo(elements);
 });
 
-function addCharsLeftEventTo(element) {
-    const $textArea = $(`#${element.elementId}`);
-    const $outputArea = $(`#${element.charsContainer}`);
+function createElements() {
+    const title = new InputArea('id_title', 'title_chars_left', addCharsLeftEvent);
+    const description = new InputArea('id_description', 'description_chars_left', addCharsLeftEvent);
+    const steps = new InputArea('id_steps', 'steps_chars_left', addCharsLeftEvent);
+
+    return [title, description, steps]
+}
+
+function addEventsTo(elements) {
+    elements.forEach((element) => element.applyEvent());
+}
+
+function addCharsLeftEvent() {
+    const $textArea = $(`#${this.elementId}`);
+    const $outputArea = $(`#${this.charsContainer}`);
     $textArea.on('input', function() {
         showCharsLeft($textArea, maxLengthOf($textArea), $outputArea)
     });
